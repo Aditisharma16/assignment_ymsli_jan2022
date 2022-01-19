15f00634afcf81a683f956a40fcfa9efaef4e518
Aditi Sharma <VE00YM341@YMI.COM>
2022-01-19 13:40:22 +0530

    day6 q1

--- Day6/src/com/Day6Q1/Q1.java
+++ Day6/src/com/Day6Q1/Q1.java
@@ -1,10 +1,54 @@
 package com.Day6Q1;
 
+import java.util.*;
+
 public class Q1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	List<Integer> list=new ArrayList<Integer>(); 
 
-	}
+	//add operation
+	list.add(1);
+	list.add(2);
+	list.add(3);
+	System.out.println(list);
+	//size operation
+		int size=list.size();
+		System.out.println("size is: "+size);
+		
+		//iterator
+		
+		 ListIterator<Integer> numIterator = list.listIterator();
 
+     // Traversing elements
+     while (numIterator.hasNext()) {
+         System.out.println(numIterator.next());
+     }
+	
+	// add all operation
+	List<Integer> list2 = new ArrayList<>();
+	list2.addAll(list);
+	System.out.println(list2);
+	
+	//remove operation
+	list.remove(1);
+	System.out.println(list);
+	
+	//remove all operation
+	list2.removeAll(list);
+	System.out.println(list2);
+	
+	
+	//contains
+	
+	boolean ans = list.contains(2);
+	  
+    if (ans)
+        System.out.println("The list contains 2");
+    else
+        System.out.println("The list does not contains 2");
+
+
+	//list.clear();
+}
 }
